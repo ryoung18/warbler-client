839b225fbb8459c2c093552ff1503f2339b3b5ad
Renwick Young <young.renwick@gmail.com>
2017-10-26 21:12:18 -0700

    JWT request token, auto submit token in header per Axios API call

--- src/WarblerContainer.js
+++ src/WarblerContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Warbler from "./Warbler";
 import axios from "axios";
+import { setAuthorizationToken } from "./setAuthorizationToken";
+
 
 class WarblerContainer extends Component {
   constructor(props) {
@@ -13,7 +15,7 @@ class WarblerContainer extends Component {
 
   componentDidMount() {
     // axios
-    //   .post("http://localhost:3005/users", {
+    //   .post("http://localhost:3000/users", {
     //     email: "hello@hello.com",
     //     username: "hello",
     //     name: "hello",
@@ -23,14 +25,14 @@ class WarblerContainer extends Component {
     //     console.log(v);
     //   });
     //   axios
-    //     .post("http://localhost:3005/warblers/1", {
+    //     .post("http://localhost:3000/warblers/1", {
     //       message: "test msg"
     //     })
     //     .then(v => {
     //       console.log(v);
     //     });
     // }
-    // axios.get("http://localhost:3005/users/1").then(v => {
+    // axios.get("http://localhost:3000/users/1").then(v => {
     //   let userWarblers = v.data.messages.map(w => {
     //     return {
     //       username: v.data.username,
@@ -43,28 +45,34 @@ class WarblerContainer extends Component {
     //   });
     // });
 
+    // Create token and store in localStorage
     axios
-      .post("http://localhost:3005/users/auth", {
+      .post("http://localhost:3000/users/auth", {
         username: "hello",
         password: "hello"
       })
-      .then(v => {
-        console.log(v);
+      .then(res => {
+        const token = res.data.token;
+        localStorage.setItem('token', token)
+
+    //     // After setting authorization token, new API calls will autmaically have the token included in the header.
+        setAuthorizationToken(token);  //<-- setting auth token.
+        // axios
+        //     .post("http://localhost:3000/warblers/45", {
+        //       message: "Yes, TokensORKS!"
+        //     })
+        //     .then(v => {
+        //       console.log(v);
+        //     });
       });
 
-      // axios
-      //   .post("http://localhost:3005/warblers/9", {
-      //     message: "test5 msg5 "
-      //   })
-      //   .then(v => {
-      //     console.log(v);
-      //   });
-    // }
-    // axios.get("http://localhost:3005/warblers/1/1").then(v => {
+
+
+    // axios.get("http://localhost:3000/warblers/1/1").then(v => {
     //   debugger
     //   console.log(v);
     // });
-    // axios.get("http://localhost:3005/warblers/1").then(v => {
+    // axios.get("http://localhost:3000/warblers/1").then(v => {
     //   console.log(v);
     // });
   }
